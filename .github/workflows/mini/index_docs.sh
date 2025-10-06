9c76f3c1d4fbcba21285bfb3b8bdc276e218b34e
Victor <194116445+dodekapod@users.noreply.github.com>
2025-10-06 16:13:31 +0200

    Again ...

---
+++ .github/workflows/mini/index_docs.sh
@@ -0,0 +1,14 @@
+source venv/bin/activate
+
+OPENRAG_ADDR=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' openrag-openrag-cpu-1`
+docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' openrag-openrag-cpu-1
+
+echo "before"${OPENRAG_ADDR}"after"
+
+python3 utility/data_indexer.py \
+    -u http://${OPENRAG_ADDR}:8080 \
+    -d .github/workflows/data/simplewiki-500/ \
+    -p simplewiki-500
+
+.github/workflows/mini/wait_for_tasks_completed.sh openrag-openrag-cpu-1 8080 500
+
