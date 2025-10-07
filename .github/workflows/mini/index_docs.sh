b2affda9bb6667e44e3c065d483127e90271901d
Victor <194116445+dodekapod@users.noreply.github.com>
2025-10-07 14:07:51 +0200

    30s -> 180s

--- .github/workflows/mini/index_docs.sh
+++ .github/workflows/mini/index_docs.sh
@@ -11,7 +11,7 @@ echo "before"${OPENRAG_ADDR}"after"
 #curl http://${OPENRAG_ADDR}:8080/health_check && echo Health is ok
 #echo $?
 #docker logs --follow openrag-openrag-cpu-1
-sleep 30s 
+sleep 180s 
 #while ! curl -fs "${OPENRAG_ADDR}:${PORT}/health_check" ;
 #do
 #  echo "Waiting for OpenRag to start at ${OPENRAG_ADDR}:${PORT}"
