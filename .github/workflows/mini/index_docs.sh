09410b9ea621c36a914d0e895bfe5aa41ff79299
Victor <194116445+dodekapod@users.noreply.github.com>
2025-10-07 12:34:43 +0200

    Print logs

--- .github/workflows/mini/index_docs.sh
+++ .github/workflows/mini/index_docs.sh
@@ -10,6 +10,8 @@ echo "before"${OPENRAG_ADDR}"after"
 
 #curl http://${OPENRAG_ADDR}:8080/health_check && echo Health is ok
 #echo $?
+docker logs --follow openrag-openrag-cpu-1
+
 while ! curl -fs "${OPENRAG_ADDR}:${PORT}/health_check" ;
 do
   echo "Waiting for OpenRag to start at ${OPENRAG_ADDR}:${PORT}"
