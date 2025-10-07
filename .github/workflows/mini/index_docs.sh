61563183ce5714ca22430529d91dd2d98660bbe4
Victor <194116445+dodekapod@users.noreply.github.com>
2025-10-07 13:58:00 +0200

    Remove waiting

--- .github/workflows/mini/index_docs.sh
+++ .github/workflows/mini/index_docs.sh
@@ -11,20 +11,22 @@ echo "before"${OPENRAG_ADDR}"after"
 #curl http://${OPENRAG_ADDR}:8080/health_check && echo Health is ok
 #echo $?
 #docker logs --follow openrag-openrag-cpu-1
-
-while ! curl -fs "${OPENRAG_ADDR}:${PORT}/health_check" ;
-do
-  echo "Waiting for OpenRag to start at ${OPENRAG_ADDR}:${PORT}"
-  sleep 10s
-  OPENRAG_ADDR=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' openrag-openrag-cpu-1`
-  docker container ls
-done
+sleep 30s 
+#while ! curl -fs "${OPENRAG_ADDR}:${PORT}/health_check" ;
+#do
+#  echo "Waiting for OpenRag to start at ${OPENRAG_ADDR}:${PORT}"
+#  sleep 10s
+#  OPENRAG_ADDR=`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' openrag-openrag-cpu-1`
+#  docker container ls
+#done
 
 
 python3 utility/data_indexer.py \
     -u http://${OPENRAG_ADDR}:8080 \
     -d .github/workflows/data/simplewiki-500/ \
     -p simplewiki-500
+
+docker logs openrag-openrag-cpu-1
 
 .github/workflows/mini/wait_for_tasks_completed.sh openrag-openrag-cpu-1 8080 500
 
